1d44d17244e209487f00d10ab0e5ad28f43aea5c
dcarruth <car12047@byui.edu>
2018-07-26 21:57:28 -0600

    Started the map activity. Got a map API key. Had to update Android studio, hope that does not screw you guys up...

--- app/src/main/java/com/example/carruth/silenceit/MainActivity.java
+++ app/src/main/java/com/example/carruth/silenceit/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.carruth.silenceit;
 
+import android.content.Intent;
+import android.location.Location;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Location location;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(this, Map.class);
+        intent.putExtra("location",location);
+        startActivity(intent);
     }
 }
